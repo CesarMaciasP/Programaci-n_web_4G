bd4785ff3b13fe84f40f684df5167fc6def5d4e0
CesarMaciasP <cmacias@ucol.mx>
2022-02-27 19:53:08 -0600

    Prueba de chalk 2

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import chalk from "chalk"
 
 console.log(chalk.blue('Hello world!'));
+
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
