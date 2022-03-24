f2233dad65a98a01b82cc10407e57e74e0280ef9
CesarMaciasP <cmacias@ucol.mx>
2022-03-23 22:14:54 -0600

    Actividad #10 realizada

---
+++ practica_10/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express(); //Declaramos app de express
+
+var port = process.env.PORT || 3000; //Abrimos un puerto para el servidor
+
+//Primera ruta. Al nivel de la raíz
+app.get('/', function (req, res){
+    res.send('<hmtl><head></head><body><h1>Hello world!</h1></body></html>');
+});
+
+//Segunda ruta. Regresa un objeto JSON
+app.get('/api', function(req, res){
+    res.json({firstname: 'John', lastname:'Doe'});
+});
+
+//Tercera ruta. Recibe un parametro
+app.get('/person/:id', function (req, res) {
+    res.send('<hmtl><head></head><body><h1>Person: ' + req.params.id + ' </h1></body></html>')
+});
+
+app.listen(port); //Iniciar el sever y ponerlo a escuchar
