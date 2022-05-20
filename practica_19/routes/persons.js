a7957ae0b9d910bad345e951020661aa9a315414
CesarMaciasP <cmacias@ucol.mx>
2022-05-20 17:47:59 -0500

    Actividad ÂÂ#19 realizada

---
+++ practica_19/routes/persons.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Person = require("../models/person");
+
+router.get("/persons", function (req, res, next) {
+  Person.find( function (err, persons) {
+    if (err) return next(err);
+    res.json(persons);
+  });
+});
+
+router.post("/persons", async (req, res, next) => {
+  const { fname, lname } = req.body;
+  const newPerson = new Person({ firstName: fname, lastName: lname });
+  console.log(newPerson);
+  await newPerson.save();
+  res.send(`First name es: ${fname} Last name es: ${lname}`);
+  console.log(req.body);
+});
+
+module.exports = router;
