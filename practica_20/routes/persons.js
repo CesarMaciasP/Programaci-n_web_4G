71f25e70ea57d3186c5fe026dc6a0e8c83789b23
CesarMaciasP <cmacias@ucol.mx>
2022-05-23 22:01:06 -0500

    Actividad #20 realizada

---
+++ practica_20/routes/persons.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('../node_modules/mongoose');
+let Person = require('../models/person');
+
+ 
+
+    router.get('/person', function(req, res){
+        res.render('person');
+    });
+
+    router.post('/addPerson', function(req, res){
+ 
+        const myPerson = new Person({
+            nombre: req.body.nombre,
+            edad: req.body.edad,
+            tipoSangre: req.body.tipoSangre,
+            nss: req.body.nss});// crea la entidad
+        myPerson.save();//guarda en bd
+    });
+
+    router.get('/persons', function(req,res,next) {
+      Person.find(function(err,person){
+          if(err) return next(err);
+          res.json(person);
+      });
+  });
+    
+module.exports=router;
---
+++ practica_20/server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const personsRoutes = require('./routes/persons');
+
+mongoose.Promise = global.Promise;
+const app = express();
+app.set('view engine','ejs');
+app.use(express.urlencoded( { extended: false } ));
+app.use(personsRoutes);
+
+mongoose.connect(
+    `mongodb+srv://usuario:contraseña321@cluster0.jlyzd.mongodb.net/?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }    
+);
+
+const db = mongoose.connection;
+db.on("erros", console.error.bind(console, "Error de conexión"));
+db.once("open", function () {
+    console.log("Conectado al servidor");
+});
+
+app.listen(3000)
