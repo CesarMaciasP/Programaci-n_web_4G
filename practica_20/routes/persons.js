a8cee0ce012986cbe5f5c8aaf9cf993846baf5c3
CesarMaciasP <cmacias@ucol.mx>
2022-05-29 15:34:43 -0500

    Actividad #22 realizada

--- practica_20/routes/persons.js
+++ practica_20/routes/persons.js
@@ -29,5 +29,31 @@ let Person = require('../models/person');
           res.render('personsIndex', {persons});
       });
   });
+
+  router.get('/deletePerson/:id', function (req, res, next){
+      Person.findByIdAndRemove(req.params.id, req.body, function (err, post){
+          if(err) return next (err);
+          res.redirect('/persons');
+      })
+  });
+
+  router.get('/findById/:id', function(req, res, next){
+      Person.findById(req.params.id, function(err, person){
+        if(err) return next (err);
+        res.render('personUpdate', {person});
+      });
+  })
+
+  router.post('/updatePerson', function(req, res, next) {
+      Person.findByIdAndUpdate(req.body.objId, {
+          nombre: req.body.nombre,
+          edad: req.body.edad,
+          tipoSangre: req.body.tipoSangre,
+          nss: req.body.nss }, function (err, post) {
+        if (err) return next(err);
+        res.redirect('/persons');
+          });
+      });
+
     
 module.exports=router;
